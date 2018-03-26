9942871bacaa9ebb642092a4daee4e5a5dc9d062
chesterosoronyas <chester.osoro@thepalladiumgroup.com>
2018-03-26 08:18:51 +0300

    Initial Commit
    Initial Commit

---
+++ KNH IQVL/IQVL/BusinessLayer/clsGbl.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IQVL.BusinessLayer
+{
+    public class clsGbl
+    {
+        public static string DBSecurity = "'ttwbvXWpqb5WOLfLrBgisw=='";
+        public static bool SettingsValid = true;
+        //EMR user
+        public static User loggedInUser = new User();
+        public static string PatientID;
+        public static DataTable dr;
+        public static DataTable AddList;
+        public static bool firstload = true;
+        public static Form frm;
+        public static Form frmMain;
+        public static DataGridView dgvView;
+        public static Form frmViralloadlist;
+
+
+        public static DataTable VLDataTable;
+        public static DataTable VLWithoutDataTable;
+
+        public  static string MakeDate(string theDate)
+        {
+            string date = theDate;
+            if (theDate != "")
+            {
+                string theDay, theMonth, theYear;
+                string[] theDT, theDT2;
+                theDT = theDate.Split(Convert.ToChar("-"));
+                if (theDT.Length > 1)
+                {
+                    theDay = theDT[0];
+                    theMonth = theDT[1];
+                    theYear = theDT[2];
+                }
+                else
+                {
+                    theDT = theDate.Split(Convert.ToChar("/"));
+                    theDay = theDT[1];
+                    theMonth = theDT[0];
+                    theDT2 = theDT[2].Split(Convert.ToChar(" "));
+                    theYear = theDT2[0];
+                }
+
+                string.Format("{0}/{1}/{2}", theMonth, theDay, theYear);
+
+            }
+            
+                return date;
+            
+        
+
+        }
+    }
+}
---
+++ KNH IQVL/IQVL/DataLayer/CsvHelpers.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.IO;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Collections;
+namespace IQVL.DataLayer
+{
+  public static class CsvHelpers
+  {
+
+      public static DataTable ToDataTable(this Dictionary<string, List<object>> dict)
+      {
+          DataTable dataTable = new DataTable();
+
+          dataTable.Columns.AddRange(dict.Keys.Select(c => new DataColumn(c)).ToArray());
+
+          for (int i = 0; i < dict.Values.Max(item => item.Count()); i++)
+          {
+              DataRow dataRow = dataTable.NewRow();
+
+              foreach (var key in dict.Keys)
+              {
+                  if (dict[key].Count > i)
+                      dataRow[key] = dict[key][i];
+              }
+              dataTable.Rows.Add(dataRow);
+          }
+
+          return dataTable;
+      }
+      public static void ForEach<T>(
+                 this IEnumerable<T> source,
+    Action<T> action)
+      {
+          foreach (T element in source)
+              action(element);
+      }
+      public static DataTable CombineMisMatchedCsvFiles(string[] filePaths, char splitter = ',')
+      {
+
+          HashSet<string> combinedheaders = new HashSet<string>();
+          int i;
+          // aggregate headers
+          for (i = 0; i < filePaths.Length; i++)
+          {
+              string file = filePaths[i];
+              combinedheaders.UnionWith(File.ReadLines(file).First().Split(splitter));
+          }
+          var hdict = combinedheaders.ToDictionary(y => y, y => new List<object>());
+
+          string[] combinedHeadersArray = combinedheaders.ToArray();
+          for (i = 0; i < filePaths.Length; i++)
+          {
+              var fileheaders = File.ReadLines(filePaths[i]).First().Split(splitter);
+              var notfileheaders = combinedheaders.Except(fileheaders);
+
+              File.ReadLines(filePaths[i]).Skip(1).Select(line => line.Split(splitter)).ForEach(spline =>
+              {
+                  for (int j = 0; j < fileheaders.Length; j++)
+                  {
+                      hdict[fileheaders[j]].Add(spline[j]);
+                  }
+                  foreach (string header in notfileheaders)
+                  {
+                      hdict[header].Add(null);
+                  }
+
+              });
+          }
+
+          DataTable dt = hdict.ToDataTable();
+          return dt;
+        
+      }
+    }
+}
